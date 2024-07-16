149758fb86b3c010f8d3125cf669ce6d3e33e702
katungi <dankatdennis@gmail.com>
2024-07-16 18:02:05 +0300

    feat: setup vite outputs

--- packages/ui/vue/vite.config.ts
+++ packages/ui/vue/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dtsPlugin from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dtsPlugin()],
+  build: {
+    lib: {
+      entry: 'src/main.ts',
+      name: 'vue-ui',
+      fileName: (format) => `vue-ui.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['vue']
+    }
+  },
 })
